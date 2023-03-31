4a97675a299e2e725a85ed9807daa50b9c7671e4
abhishekkmwt <kumawat.abhishek167@gmail.com>
2023-03-31 16:01:45 +0530

    Assignment Done4

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,7 +9,10 @@ public class Order {
         this.id=id;
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
-        this.deliveryTime=time(deliveryTime);
+        Integer hr=Integer.valueOf(deliveryTime.substring(0,2));
+        Integer min=Integer.valueOf(deliveryTime.substring(3));
+        Integer currTime=hr*60+min;
+        this.deliveryTime=currTime;
    }
 
     public Order() {
@@ -21,21 +24,4 @@ public class Order {
     }
 
     public int getDeliveryTime() {return deliveryTime;}
-
-    public int time(String time){
-        String arr[]=time.split(":");
-        int hr=0;
-        String s=arr[0];
-        String m=arr[1];
-        int h=0;
-        for(int i=0;i<s.length();i++){
-            h=(h*10)+s.charAt(i)-'0';
-        }
-        hr=60*h;
-        int min=0;
-        for(int i=0;i<m.length();i++){
-            min=(min*10)+m.charAt(i)-'0';
-        }
-        return hr+min;
-    }
 }
